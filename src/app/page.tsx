02014df32d3547afc3f6de90ef9330cf52811a6e
HiImConan <mindol0320@naver.com>
2023-07-05 21:55:30 +0900

    6.5: Image Optimization and Loading UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,12 @@ export default function Home() {
     <>
       <h1>홈페이지다!!</h1>
       <Counter />
+      <Image
+        src="https://images.unsplash.com/photo-1441986300917-64674bd600d8"
+        alt="shop"
+        width={400}
+        height={400}
+      />
     </>
   );
 }
---
+++ src/app/products/loading.tsx
@@ -0,0 +1,3 @@
+export default function ProductsLoading() {
+  return <p>loading...</p>;
+}
