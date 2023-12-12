5b2926ca530544b62d7e358cd119a2fa6e617eda
HiImConan <mindol0320@naver.com>
2023-12-12 22:43:27 +0900

    headlessui Listbox 해부

--- wanted-challenges/week1/src/App.tsx
+++ wanted-challenges/week1/src/App.tsx
@@ -1,13 +1,13 @@
-import Route from "./components/Route";
-import Router from "./components/Router";
-import { Root } from "./pages/Root";
-import { About } from "./pages/About";
+import Route from './components/Route';
+import Router from './components/Router';
+import { Root } from './pages/Root';
+import { About } from './pages/About';
 
 export default function App() {
   return (
     <Router>
-      <Route path="/" component={<Root />} />
-      <Route path="/about" component={<About />} />
+      <Route path='/' component={<Root />} />
+      <Route path='/about' component={<About />} />
     </Router>
   );
 }
